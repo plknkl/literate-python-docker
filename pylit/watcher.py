b8da44ff0dbfa6186e690f7fd90e7c1fd1b431c7
Nikolai Polikurov <nikolai@polikurov.com>
2022-01-12 15:09:00 +0100

    nested source files support

--- pylit/watcher.py
+++ pylit/watcher.py
@@ -1,10 +1,23 @@
 import os, time
+import pathlib
 import pylit
 
 pylit.defaults.code_block_marker = '.. code-block:: python'
 pylit.defaults.comment_string = "# "
 pylit.defaults.codeindent = 4
 pylit.defaults.text_extensions = [".rst"]
+
+def pylit_render(src, dst):
+    try:
+        pylit.main([src, dst])
+    except IOError:
+        pass
+
+def create_missing_dirs(dst):
+    dst_dir = os.path.dirname(dst)
+    if not os.path.exists(dst_dir):
+        p = pathlib.Path(dst_dir)
+        p.mkdir(parents=True)
 
 def render(filepath):
     # render the two way files
@@ -14,55 +27,64 @@ def render(filepath):
     else:
         dst = src.replace('/docs/rst/', '/src/')[:-4]
         html_dst = src.replace('/rst/', '/html/')[:-4] + '.html'
+        create_missing_dirs(html_dst)
         os.system(f'rst2html.py --stylesheet ./pylit/style.css {src} {html_dst}')
     
     # in order to brake the infinite loop in which the 
     # automatic modifications trigger other modifications
     # we wait for a reasonable human intervention time
-    srctime = os.stat(src).st_mtime
-    dsttime = os.stat(dst).st_mtime
-    timedifference = abs(round(srctime - dsttime))
-    if timedifference > 2:
-        pylit.main([src, dst])
+    try:
+        srctime = os.stat(src).st_mtime
+        dsttime = os.stat(dst).st_mtime
+        timedifference = round(srctime - dsttime)
+        if timedifference > 2:
+            pylit_render(src, dst)
+    except FileNotFoundError as err:
+        create_missing_dirs(dst)
+        pylit_render(src, dst)
 
-def get_files_stats(path_to_watch, endsw):
-    results = []
-    for f in os.listdir(path_to_watch):
-        fp = f'{path_to_watch}/{f}'
-        if f.endswith(endsw):
-            results.append(
-                (
-                    os.stat(fp).st_mtime_ns,
-                    f
-                )
-            )
+def get_files_stats(tracked_list):
+    results = {}
+    for f in tracked_list:
+        results[f] = os.stat(f).st_mtime_ns
     return results
 
-def find_file_by_time(time, files):
-    return next(filter(lambda x: x[0] == time, files))[1]
+def find_modified_files(found_files_stats, tracked_files_dict):
+    modified_files = []
+    for k, v in found_files_stats.items():
+        if k not in tracked_files_dict:
+            modified_files.append(k)
+        else:
+            if v != tracked_files_dict[k]:
+                modified_files.append(k)
 
-def tracker(path_to_watch, tracked_list, ext):
-    # get all the stats of tracked files
-    next_tracked_files = get_files_stats(path_to_watch, ext)
-    # get only the actual times
-    ntf_time = map(lambda x: x[0], next_tracked_files)
-    # get the old times
-    tf_time = map(lambda x: x[0], tracked_list)
+    return modified_files
+
+def tracker(path_to_watch, tracked_files_dict, ext):
+    found_files = []
+    for root, dirs, files in os.walk(path_to_watch):
+        for file in files:
+            if file.endswith(ext):
+                found_files.append((os.path.join(root, file)))
+    # get all the stats of found files
+    found_files_stats = get_files_stats(found_files)
+
     # select only the modified files
-    refresh_files = [find_file_by_time(f, next_tracked_files) for f in ntf_time if not f in tf_time]
+    modified_files = find_modified_files(found_files_stats, tracked_files_dict)
 
-    for f in refresh_files:
-        render(f'{path_to_watch}/{f}')
+    for f in modified_files:
+        render(f)
 
-    return next_tracked_files
+    return found_files_stats
 
 py_path_to_watch = "./src"
 rst_path_to_watch = "./docs/rst"
-tracked_py_files = []
-tracked_rst_files = []
+tracked_py_files = {}
+tracked_rst_files = {}
 
 print("I'm watching your back.. go on..")
 while True:
-    time.sleep(2)
+    # find all py files
     tracked_py_files = tracker(py_path_to_watch, tracked_py_files, 'py')
     tracked_rst_files = tracker(rst_path_to_watch, tracked_rst_files, 'rst')
+    time.sleep(2)
